d8667b8e212ff12be753b320bf47a169fd573a5f
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-14 16:42:31 -0300

    alterações no corpo do dino

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def index():
     return render_template("index.html")
 
 
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT",5000))
     app.run(debug=True, host='localhost', port=port)
