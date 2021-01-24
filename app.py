ab24188c404c6651593a2711f48f5f9e231d6982
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-24 11:51:44 -0300

    resolvendo bug  error 503 azure teste 01.

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template , redirect, url_for, request
 app = Flask(__name__)
 
 #route
-@app.route("/", methods=["GET"] )
-@app.route("/index", methods=["GET"] )
+@app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
